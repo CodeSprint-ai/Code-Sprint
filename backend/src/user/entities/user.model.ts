2f636834a8368d8ea91c2fc96d65182cf886638d
Moin Uddin <s.m.moin2004@gmail.com>
2025-08-30 17:11:41 +0500

    add level at user

--- backend/src/user/entities/user.model.ts
+++ backend/src/user/entities/user.model.ts
@@ -7,25 +7,26 @@ import {
 } from 'typeorm';
 import { ProviderEnum } from 'src/auth/enum/ProviderEnum';
 import { RoleEnum } from 'src/user/enum/RoleEnum';
+import { UserLevel } from '../enum/UserLevel';
 
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @Column({ unique: true })
+  @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   name?: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   password?: string;
 
-  @Column({ default: false })
+  @Column({ type: 'boolean', default: false })
   isVerified: boolean;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   emailVerificationToken?: string;
 
   @Column({ type: 'varchar', nullable: true })
@@ -42,6 +43,9 @@ export class User {
 
   @Column({ type: 'enum', enum: RoleEnum, default: RoleEnum.USER })
   role: RoleEnum;
+
+  @Column({ type: 'enum', enum: UserLevel, default: UserLevel.BEGINNER })
+  level: UserLevel;
 
   @CreateDateColumn()
   createdAt: Date;
