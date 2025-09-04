777797a2868ddffd4b638a970822d26f58a44913
JamalNadeem <jamalnadeem2004@gmail.com>
2025-09-04 10:16:32 +0500

    feat: add ThemeProvider setup for dark mode support

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,13 +2,16 @@
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
 import '@/app/globals.css';
+import { ThemeProvider } from "./providers";
 import ReduxProvider from '@/redux/reduxProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <ReduxProvider>{children}</ReduxProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
