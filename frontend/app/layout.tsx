6b4f51e6a393459ea99071d6eb521a9caf172e7b
JamalNadeem <jamalnadeem2004@gmail.com>
2025-09-04 10:58:02 +0500

    wip: changes for toggle button to system detect theme only

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <ReduxProvider>{children}</ReduxProvider>
         </ThemeProvider>
       </body>
